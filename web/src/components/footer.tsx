cab66135d103c676a10997ea96de725bf24f8b5d
devrazz09 <hinarazelmarlonosera@gmail.com>
2026-04-18 23:03:16 +0800

    how it works frontend done

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,7 +1,52 @@
+import Link from "next/link"
+
 export default function Footer(){
     return(
-        <footer className="w-full h-[70dvh] border">
-
+        <footer className="w-full h-[70dvh] relative flex justify-center items-end bg-[url(/footerbg.png)] bg-cover bg-no-repeat bg-center font-mono">
+            <div className="w-4/5 h-[30dvh] mb-26 flex justify-between items-center">
+                <div className="w-1/3 h-full flex flex-col justify-start items-start gap-4">
+                   <div className="flex justify-start items-center">
+                        <img src="/logo.png" width={60} height={60} alt="" />
+                        <p className="text-4xl font-semibold text-textMainColor">ECO<span className="text-secondary">PESO</span></p>
+                    </div>
+                    <p className="text-2xl w-3/4 text-white">Partner with us to plant trees and nurture a healthier planet.</p>
+                </div>
+                <div className="w-2/3 h-full flex justify-start items-start gap-32">
+                    <div className="flex flex-col justify-start items-start gap-4">
+                        <div>
+                            <p className="text-3xl text-textMainColor">Navigation</p>
+                        </div>
+                        <div className="flex flex-col justify-start items-start gap-2 text-2xl text-white">
+                            <Link href={'/'}>Home</Link>
+                            <Link href={'/'}>Donate</Link>
+                            <Link href={'/'}>Merchs</Link>
+                            <Link href={'/'}>How It Works</Link>
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-start items-start gap-4">
+                        <div>
+                            <p className="text-3xl text-textMainColor">About</p>
+                        </div>
+                        <div className="flex flex-col justify-start items-start gap-2 text-2xl text-white">
+                            <Link href={'/'}>Mission & Vission</Link>
+                            <Link href={'/'}>About Ecopeso</Link>
+                            <Link href={'/'}>Our Story</Link>
+                            <Link href={'/'}>Learn More</Link>
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-start items-start gap-4">
+                        <div>
+                            <p className="text-3xl text-textMainColor">Contacts</p>
+                        </div>
+                        <div className="flex flex-col justify-start items-start gap-2 text-2xl text-white">
+                            <Link href={'/'}>Facebook</Link>
+                            <Link href={'/'}>Twitter</Link>
+                            <Link href={'/'}>Youtube</Link>
+                            <Link href={'/'}>Instagram</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
--- web/src/components/logo.tsx
+++ web/src/components/logo.tsx
@@ -1,4 +1,4 @@
-export default function logoComponent(){
+export default function LogoComponent(){
     return(
         <div className="w-fit h-fit flex items-center justify-between font-mono font-bold">
             <img src="/logo.png" alt="logo" fetchPriority="high" loading="lazy"/>
