c3d3b5fea96ca2cd376434f2a01fa469a48f3fe6
devrazz09 <hinarazelmarlonosera@gmail.com>
2026-04-20 18:38:09 +0800

    to deploy

--- web/src/components/merchandisecard.tsx
+++ web/src/components/merchandisecard.tsx
@@ -7,8 +7,8 @@ interface ProductProps {
 
 export default function MerchandiseCard({ image, name, price, description } : ProductProps ){
     return(
-        <div className="w-[49%] h-167 flex flex-col justify-between gap-2 rounded-[20px] shadow-[0px_0px_5px_1px_rgba(162,162,162,0.8)] p-6 font-semibold">
-            <img className="w-full h-[55%]" src={image} alt="" />
+        <div className="w-[49%] h-177 flex flex-col justify-between gap-2 rounded-[20px] shadow-[0px_0px_5px_1px_rgba(162,162,162,0.8)] p-6 font-semibold">
+            <img className="w-full h-[55%] object-cover rounded-xl" src={image} alt="" />
             <p className="text-[40px] font-bold">{name}</p>
             <p className="text-2xl">₱{price}.00</p>
             <p className="text-xl">{description}</p>
